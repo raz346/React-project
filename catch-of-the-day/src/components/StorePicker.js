653bae46b520ace1e2cfedff74efb730c37ef663
Ahmed Alhashimi <alhashimi346@gmail.com>
2019-09-12 14:05:01 -0600

    add storepicker class and load css

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <form action="" className="store-selector">
+          <h2>please enter a store</h2>
+          <input type="text" required placeholder= "Store Name"/>
+          <button type="submit">Visit Store -></button>
+        </form>
+      </Fragment>
+    )
+  }
+}
+
+export default StorePicker;
