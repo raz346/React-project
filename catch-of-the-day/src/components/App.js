97e9d44091d0e9c7bdc725a9473a67c130e36673
Ahmed Alhashimi <alhashimi346@gmail.com>
2019-09-12 22:29:50 -0600

    remove header

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
 class App extends React.Component {
   render() { 
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline='A Cool Market'/>
         </div>
-        {/* <Inventory /> */}
-        {/* <Order /> */}
+        <Order />
+        <Inventory />
       </div>
     );
   }
